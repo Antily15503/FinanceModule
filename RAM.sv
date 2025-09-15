5d4f807f264364469bf999e820369cd721626080
Ethan Li <yli2494@wisc.edu>
2025-09-14 21:48:36 -0500

    started a RAM implementation

---
+++ RAM.sv
@@ -0,0 +1,10 @@
+module RAM #(parameter USER_CAPACITY, parameter ASSET_COUNT)(
+    input wire [15:0] name,
+    input wire 
+)
+parameter LOG = $clog(USER_CAPACITY);
+
+
+endmodule
+
+module RAM_Instruction_fetch
